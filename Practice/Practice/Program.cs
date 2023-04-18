59e99c46594698cb69c85d403aa7fe8ca1aa77e7
Chinara0303 <chinaraei@code.edu.az>
2023-04-18 18:25:37 +0400

    initial

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Practice.Data;
+using Practice.Services;
+using Practice.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.AddDbContext<AppDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<IBlogService, BlogService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.AddScoped<ILayoutService, LayoutService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ISliderService, SliderService>();
+builder.Services.AddScoped<IExpertService, ExpertService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseSession();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Practice/Practice/ViewModels/Layout/LayoutVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice.ViewModels.Layout
+{
+    public class LayoutVM
+    {
+        public Dictionary<string,string> Settings { get; set; }
+        public int  BasketCount { get; set; }
+
+    }
+}
